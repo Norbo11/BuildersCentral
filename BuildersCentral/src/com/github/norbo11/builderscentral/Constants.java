26030c288a39b5c7a0288f09dd75036501576441
Norbo11 <norbo11@googlemail.com>
2014-07-10 14:53:51 +0100

    Second Commit

---
+++ BuildersCentral/src/com/github/norbo11/builderscentral/Constants.java
@@ -0,0 +1,5 @@
+package com.github.norbo11.builderscentral;
+
+public class Constants {
+    public static final String APPLICATION_NAME = "BuildersCentral";
+}
---
+++ BuildersCentral/src/com/github/norbo11/builderscentral/LoginController.java
@@ -0,0 +1,10 @@
+package com.github.norbo11.builderscentral;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML private void submitLogin(ActionEvent event) {
+        System.out.println("clicked");
+    }
+}
